09a23dda9fb68ea7f485a2dee1734e1710715bbf
Guilherme Batista <Guilherme.batista.3000@gmail.com>
2026-05-04 18:11:27 -0300

    feat: add Order and OrderItem models

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 from django.utils.text import slugify
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -37,3 +38,26 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.name} - {self.price}'
+
+
+class Order(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='orders')
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return f'Order {self.id} - {self.user.username}'
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(
+        validators=[MinValueValidator(1)])
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return f'{self.product.name} X {self.quantity}'
