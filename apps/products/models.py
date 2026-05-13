a17319de380dcdb22daa59d9c5a3a1bb7d431b98
Guilherme Batista <Guilherme.batista.3000@gmail.com>
2026-05-13 14:31:54 -0300

    Implement inventory and product ingredient models

--- apps/products/models.py
+++ apps/products/models.py
@@ -64,3 +64,30 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return f'{self.product.name} X {self.quantity}'
+
+
+class Ingredient(models.Model):
+    UNITS = (
+        ('g', 'Grams'),
+        ('kg', 'Kilograms'),
+        ('ml', 'Milliliters'),
+        ('l', 'Liters'),
+    )
+
+    name = models.CharField(max_length=100,)
+    stock_quantity = models.DecimalField(
+        default=0, max_digits=10, decimal_places=2)
+    unit = models.CharField(max_length=20, choices=UNITS)
+    minimum_stock = models.DecimalField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class ProductIngredient(models.Model):
+    product = models.ForeignKey(
+        Product, related_name='ingredients', on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(
+        Ingredient, related_name='products', on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        unique_together = ['product', 'ingredient']
