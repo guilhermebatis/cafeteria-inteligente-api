dcdf79da551db4ffdefe758f37fa4657cf90b600
Guilherme Batista <Guilherme.batista.3000@gmail.com>
2026-05-16 18:13:14 -0300

    test: add finalize_order service tests

--- apps/products/tests/test_services.py
+++ apps/products/tests/test_services.py
@@ -12,25 +12,79 @@ class FinalizeOrderTest(TestCase):
             username='test',
             password='123'
         )
-
-    def test_finalize_order_success(self):
-        category = Category.objects.create(
+        self.category = Category.objects.create(
             name='Test Category'
         )
-        ingredient = Ingredient.objects.create(
-            name="Test Ingredient", stock_quantity=100)
-        product = Product.objects.create(
-            name="Test Product", price=10.00,
-            is_available=True, category=category)
-        ProductIngredient.objects.create(
-            product=product, ingredient=ingredient, quantity=2
+        self.ingredient = Ingredient.objects.create(
+            name="Test Ingredient", stock_quantity=100
         )
+        self.product = Product.objects.create(
+            name="Test Product", price=10.00,
+            is_available=True, category=self.category
+        )
+        self.product_ingredient = ProductIngredient.objects.create(
+            product=self.product, ingredient=self.ingredient, quantity=2
+        )
+        self.order = Order.objects.create(user=self.user)
 
-        order = Order.objects.create(user=self.user)
-        order.items.create(product=product, quantity=3, price=30.00)
-        finalize_order(order)
-        order.refresh_from_db()
-        ingredient.refresh_from_db()
-        self.assertTrue(order.is_completed)
-        self.assertEqual(ingredient.stock_quantity, Decimal('94.00'))
+    def test_finalize_order_success(self):
+        self.order.items.create(product=self.product, quantity=3, price=30.00)
+        finalize_order(self.order)
+        self.order.refresh_from_db()
+        self.ingredient.refresh_from_db()
+        self.assertTrue(self.order.is_completed)
+        self.assertEqual(self.ingredient.stock_quantity, Decimal('94.00'))
         self.assertEqual(StockMovement.objects.count(), 1)
+
+    def test_finalize_order_insufficient_stock(self):
+        self.ingredient.stock_quantity = 1
+        self.ingredient.save()
+        self.order.items.create(product=self.product, quantity=3, price=30.00)
+        with self.assertRaises(ValueError):
+            finalize_order(self.order)
+        self.order.refresh_from_db()
+        self.ingredient.refresh_from_db()
+        self.assertFalse(self.order.is_completed)
+        self.assertEqual(self.ingredient.stock_quantity, Decimal('1.00'))
+        self.assertEqual(StockMovement.objects.count(), 0)
+
+    def test_order_finalized(self):
+        self.order.items.create(product=self.product, quantity=3, price=30.00)
+        finalize_order(self.order)
+        with self.assertRaises(ValueError):
+            finalize_order(self.order)
+        self.order.refresh_from_db()
+        self.ingredient.refresh_from_db()
+        self.assertEqual(self.ingredient.stock_quantity, Decimal('94.00'))
+        self.assertTrue(self.order.is_completed)
+        self.assertEqual(StockMovement.objects.count(), 1)
+
+    def test_acumulator_stock_insufficient(self):
+        self.ingredient = Ingredient.objects.create(
+            name="milk", stock_quantity=10
+        )
+        self.ingredient.save()
+        product_milk = Product.objects.create(
+            name="Milk Product", price=10.00,
+            is_available=True, category=self.category
+        )
+        product_coffe_with_milk = Product.objects.create(
+            name="Coffe with Milk", price=15.00,
+            is_available=True, category=self.category
+        )
+        self.product_ngredient = ProductIngredient.objects.create(
+            product=product_milk, ingredient=self.ingredient, quantity=5
+        )
+        self.product_ngredient = ProductIngredient.objects.create(
+            product=product_coffe_with_milk, ingredient=self.ingredient, quantity=5
+        )
+        self.order.items.create(product=product_milk, quantity=1, price=10.00)
+        self.order.items.create(
+            product=product_coffe_with_milk, quantity=2, price=30.00)
+        with self.assertRaises(ValueError):
+            finalize_order(self.order)
+        self.order.refresh_from_db()
+        self.ingredient.refresh_from_db()
+        self.assertFalse(self.order.is_completed)
+        self.assertEqual(self.ingredient.stock_quantity, Decimal('10.00'))
+        self.assertEqual(StockMovement.objects.count(), 0)
