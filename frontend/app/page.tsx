a7f2e78c663ef1f0f7c794a94529b9214a28b7c5
Guilherme Batista <Guilherme.batista.3000@gmail.com>
2026-05-18 19:47:23 -0300

    feat: implement frontend authentication and protected routes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 interface Category {
   id: number;
@@ -17,11 +18,30 @@ interface Product {
 }
 
 export default function Home() {
+  const router = useRouter();
   const [products, setProducts] = useState<Product[]>([]);
+
+  function handleLogout() {
+  localStorage.removeItem("access");
+  localStorage.removeItem("refresh");
+
+  router.push("/login");
+}
+
+  useEffect(() => {
+    const token = localStorage.getItem("access");
+
+      if (!token) {
+        router.push("/login");
+      }
+    }, []);
 
   useEffect(() => {
     async function fetchProducts() {
       const token = localStorage.getItem("access");
+
+      if (!token) return;
+
       const response = await fetch("http://127.0.0.1:8000/api/products/", {
         headers: {
           Authorization: `Bearer ${token}`
@@ -38,6 +58,13 @@ export default function Home() {
 
   return (
     <main className="p-10">
+
+        <button
+          onClick={handleLogout}
+          className="mb-6 border px-4 py-2 rounded"
+          >
+            Logout
+        </button>
       <h1 className="text-3xl font-bold mb-6">
         Cafeteria Inteligente
       </h1>
