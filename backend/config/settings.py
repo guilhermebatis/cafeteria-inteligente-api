7f285897ed0942f7c7bb4c71bc9100f750af4806
Guilherme Batista <Guilherme.batista.3000@gmail.com>
2026-06-06 21:57:37 -0300

    dockerize fullstack application with PostgreSQL and Docker Compose

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -105,10 +105,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cafeteria_db',
+        'USER': 'cafeteria_user',
+        'PASSWORD': 'cafeteria_password',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
