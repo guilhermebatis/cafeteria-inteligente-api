2d9e0cfdb93168e97ce84366ccd4e9c7b3140711
Guilherme Batista <Guilherme.batista.3000@gmail.com>
2026-07-08 22:27:43 -0300

    refactor: reorganize Next.js layouts and navbar structure

--- frontend/app/(site)/layout.tsx
+++ frontend/app/(site)/layout.tsx
@@ -1,25 +1,15 @@
 "use client";
-import NavbarProvider from "@/components/LayoutClient";
 
+import NavbarProvider from "@/components/LayoutClient";
 
 export default function SiteLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-
-
     return (
-        <main className="flex min-h-screen">
-
-            <NavbarProvider>
-
-                <div className="flex-1 p-10">
-                    {children}
-                </div>
-
-            </NavbarProvider>
-
-        </main>
+        <NavbarProvider>
+            {children}
+        </NavbarProvider>
     );
 }
