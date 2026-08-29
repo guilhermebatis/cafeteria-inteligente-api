53d3c9438270982f2fd9f6d268a12a5509f9d864
Guilherme Batista <Guilherme.batista.3000@gmail.com>
2026-08-29 05:44:22 -0300

    feat: add coupon model

---
+++ backend/apps/coupons/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils import timezone
+from django.core.exceptions import ValidationError
+
+
+class Coupons(models.Model):
+
+    code = models.CharField(max_length=50, unique=True)
+    max_discount_value = models.DecimalField(
+        max_digits=10, decimal_places=2,
+        default=0.00, validators=[MinValueValidator(0)])
+
+    discount_percent = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0.00,
+        validators=[MaxValueValidator(100), MinValueValidator(0)])
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    expired = models.DateTimeField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def clean(self) -> None:
+        if self.expired:
+            if self.expired < timezone.now():
+                raise ValidationError(
+                    'Nao e possivel fazer um cupom com a data de experado que seja menor que hoje')
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'django_filters',
     'drf_spectacular',
     'corsheaders',
+    'apps.coupons'
 ]
 
 REST_FRAMEWORK = {
